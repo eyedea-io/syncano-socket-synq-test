95b2bb31b5537b43de9e6de180a9724fc117873a
kkkowalski <karol.kowalski@eyedea.io>
2016-11-15 10:10:39 +0100

    [SYNCONS-50] Added duration, size, width and height information on Synq meta update

--- scripts/webhook.py
+++ scripts/webhook.py
@@ -1,5 +1,6 @@
 import requests
 import syncano
+import json
 from syncano.models import Object
 
 print META["request"]
@@ -8,15 +9,30 @@ connection = syncano.connect(
     api_key=CONFIG["SYNCANO_API_KEY"],
     user_key=CONFIG['SYNCANO_USER_TOKEN'],
     instance_name=CONFIG["SYNCANO_INSTANCE_NAME"])
-    
+
 data = ARGS
+
+synq_file_details = json.loads(META["request"]["HTTP_VIDEO_INPUT"])
 
 list_of_objects = Object.please.list(
     class_name=CONFIG["VIDEO_STORAGE"]).filter(synq_video_id=META["request"]["HTTP_VIDEO_ID"])
 
 for item in list_of_objects:
-    Object.please.update(
-        id=item.id,
-        synq_state=META["request"]["HTTP_VIDEO_STATE"],
-        class_name=CONFIG["VIDEO_STORAGE"]
-    )
+    if 'duration' in synq_file_details:
+        Object.please.update(
+            id=item.id,
+            synq_state=META["request"]["HTTP_VIDEO_STATE"],
+            size=synq_file_details['file_size'],
+            duration=synq_file_details['duration'],
+            height=synq_file_details['height'],
+            width=synq_file_details['width'],
+            class_name=CONFIG["VIDEO_STORAGE"]
+        )
+    else:
+        Object.please.update(
+            id=item.id,
+            synq_state=META["request"]["HTTP_VIDEO_STATE"],
+            class_name=CONFIG["VIDEO_STORAGE"]
+        )
+
+set_response(HttpResponse(status_code=200, content='Response parsed', content_type='plain/text'))
