207828e7983b832d250a0e24d9c6f612119ba2fe
kkotelczuk <konrad.kotelczuk@gmail.com>
2017-02-10 16:28:20 +0100

    feat(tests) add passing test, extend test timeout

--- syncano/synq/tests/index.js
+++ syncano/synq/tests/index.js
@@ -23,8 +23,9 @@ const s = function(endpoint, key, params, method="POST") {
   return syncanoCall(endpoint, key, params, method)
 }
 
-describe('Synq', function() {
+describe('Synq socket', function() {
   let token = ''
+  let objectId = 0
 
   it('create or login user in Syncano', function(done) {
     const params = {
@@ -46,38 +47,41 @@ describe('Synq', function() {
   })
 
   it('upload video via uploader', function(done) {
-    s('synq/uploader/', token)
-      .then(resp => {
-        done()
-      })
+    assert.eventually.property(s('synq/uploader/', token), 'uploader_url').notify(done)
   })
 
   it('list user\'s videos', function(done) {
     s('synq/list/', token)
-      .then(resp => {
-        done()
-      })
+    .then(response => {
+      assert.isArray(response)
+      objectId = response[0].id
+      done()
+    })
+    .catch(err => done(err))
   })
 
   it('get video object details', function(done) {
-    s('synq/detials/', token, data)
-      .then(resp => {
-        done()
-      })
+    const params = {
+      id: objectId
+    }
+    assert.eventually.property(s('synq/details/', token, params), 'video_id').notify(done)
   })
 
   it('update custom user\'s data', function(done) {
-    s('synq/upload/', token, data)
-      .then(resp => {
-        done()
-      })
+    const params = {
+      id: objectId,
+      payload: {
+        test: "new test filed"
+      }
+    }
+    assert.eventually.equal(s('synq/update/', token, params), 'Success').notify(done)
   })
 
   it('delete selected object', function(done) {
-    s('synq/delete/', token, data)
-      .then(resp => {
-        done()
-      })
+    const params = {
+      id: objectId
+    }
+    assert.eventually.equal(s('synq/delete/', token, params), 'Success').notify(done)
   })
 
 })
